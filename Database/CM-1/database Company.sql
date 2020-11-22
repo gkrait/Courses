f8f9fdd26bee98832ae4fae0a15dbc404296d161
KRAIT George <george.krait@inria.fr>
2020-11-22 12:04:27 +0100

    update

---
+++ Database/CM-1/database Company.sql
@@ -0,0 +1,29 @@
+create database Company; 
+
+
+drop database   Company; 
+
+use Company; 
+
+
+
+
+create table Employee ( 
+ID int,
+Name varchar(50),
+First_name varchar(50),
+Salary  int 
+
+);
+
+
+
+insert into Employee values (123,"Sam",2000)
+
+create table Employee ( 
+ID int PRIMARY KEY,
+Name varchar(50),
+First_name varchar(50),
+Salary  int 
+
+)
